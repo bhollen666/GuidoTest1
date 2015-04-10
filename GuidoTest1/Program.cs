48c8b1be785b99c7e233862f46cf7bb4498b582a
Bob Hollen <bhollen666@gmail.com>
2015-04-09 21:40:20 -0400

    Original Files
    Original collection of files.

---
+++ GuidoTest1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GuidoTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long lngValue = 5;
+            string strValue = lngValue.ToString();
+        }
+    }
+}
